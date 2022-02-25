7a7a0a59a7f22f3b9cc60e819359ebdff09769b9
dhanyaas <98949368+dhanyaas@users.noreply.github.com>
2022-02-25 23:26:11 +0530

    updated2a.sh
    comments line are added (which means question is added )

--- shellprgms/2a.sh
+++ shellprgms/2a.sh
@@ -1,3 +1,6 @@
+#Write a shell script that accepts two filenames as arguments, checks if the permissions for 
+#these  files  are  identical  and  if  the  permissions  are  identical,  output  common  permissions 
+#otherwise output each filename followed by its permissions
 echo -n "File a file name1:"
 read f1
 if [ -e $f1 ]
