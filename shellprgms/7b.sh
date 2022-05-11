88758d5f022115acaf3454e75ee809a01fea0b37
Dhanyashree A <98949368+dhanyaas@users.noreply.github.com>
2022-05-11 08:17:35 +0530

    Correct 7b.sh with error sentences

--- shellprgms/7b.sh
+++ shellprgms/7b.sh
@@ -1,15 +1,59 @@
 #Write a shell script that accepts a list of filenames as its argument, count and report occurrence of each word that is present in the first argument file on other argument files.
 
+if [ $# -ge 2 ]
 
-for fn in $*
-   do
-	if [ $fn = $1 ]
-	 then
-		continue 
-	fi
-                     for w in `cat $1`
-                     do
-      	                 count=`grep -iwo "$w" $fn| wc -l`
-     	                 echo "$w is $count times in $fn"
-                     done
-  done
+then
+
+    for fn in $*
+
+    do
+
+        if [ -e $fn ]
+
+        then 
+
+            if [ $fn = $1 ]
+
+            then
+
+                continue
+
+            fi
+
+            for w in `cat $1`
+
+            do
+
+                lc=`grep -woi "$w" $fn | wc -l`
+
+                echo "$w is $lc in $fn"
+
+            done
+
+        else
+
+            echo "$fn File not Found!"
+
+        fi
+
+    done
+
+else
+
+    echo "ParameterPattern: PatternFile FilesToCheck.. "
+
+fi
+
+
+
+
+	
+	
+		
+	
+                    
+                     
+      	                 
+     	               
+          
+
